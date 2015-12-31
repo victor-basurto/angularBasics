481f5e4ccf916e1600bc6278b582fae5017cef77
victor-basurto <my.mvplace@gmail.com>
2015-12-30 21:57:05 -0800

    comments added to code

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,15 @@
+// Angular Module
 var myNameSpace = angular.module('myApp', []);
 
 myNameSpace.controller('MyController', ['$scope', '$http', function($scope, $http) {
-	// this is the model
+	/**
+	 * This might not work on Chrome, because of the protocol
+	 * disable chrome protocols to accept http request from 
+	 * external files without request these files from server.
+	 * Or you can use a different web browser
+	 */
 	$http.get('./js/data.json').success(function(data) {
+		// return this data.
 		$scope.artist = data;
 		$scope.artistOrder = 'name';
 	});	
